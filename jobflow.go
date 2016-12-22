83cd2f471eb93c8ebf854c0cf3cb8d57c8d2e212
Ravenwater <theo@stillwater-sc.com>
2016-12-22 15:50:38 -0500

    Simple doc edits for godoc formating

--- jobflow.go
+++ jobflow.go
@@ -1,23 +1,23 @@
-/**
- * File		:	$File: //depot/stillwater/perfmonitor/jobflow.go $
- *
- * Authors	:	E. Theodore L. Omtzigt
- * Date		:	5 May 2016
- *
- * Source Control Information:
- * Version	:	$Revision: #1 $
- * Latest	:	$Date: 2016/05/05 $
- * Location	:	$Id: //depot/stillwater/perfmonitor/jobflow.go#1 $
- *
- * Organization:
- *		Stillwater Supercomputing, Inc.
- *		P.O Box 720
- *		South Freeport, ME 04078-0720
- *
- * Copyright (c) 2006-2016 E. Theodore L. Omtzigt.  All rights reserved.
- *
- * Licence      : Stillwater license as defined in this directory
- *
+/*
+ File		:	$File: //depot/stillwater/perfmonitor/jobflow.go $
+
+ Authors	:	E. Theodore L. Omtzigt
+ Date		:	5 May 2016
+
+ Source Control Information:
+ Version	:	$Revision: #1 $
+ Latest		:	$Date: 2016/05/05 $
+ Location	:	$Id: //depot/stillwater/perfmonitor/jobflow.go#1 $
+
+ Organization:
+		Stillwater Supercomputing, Inc.
+		P.O Box 720
+		South Freeport, ME 04078-0720
+
+Copyright (c) 2006-2016 E. Theodore L. Omtzigt.  All rights reserved.
+
+Licence      : Stillwater license as defined in this directory
+
  */
 package perfmonitor
 
